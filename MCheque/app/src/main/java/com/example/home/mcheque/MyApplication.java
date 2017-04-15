14a8dc46a5fdb89ead8212d786dcb077da6c643f
komall-webonise <komal493549@gmail.com>
2017-04-16 01:49:45 +0530

    added the participant mapping api

--- MCheque/app/src/main/java/com/example/home/mcheque/MyApplication.java
+++ MCheque/app/src/main/java/com/example/home/mcheque/MyApplication.java
@@ -2,8 +2,13 @@ package com.example.home.mcheque;
 
 import android.app.Application;
 
+import com.example.home.mcheque.participantDataMapping.CustomerDetails;
+
+import java.util.List;
+
 public class MyApplication extends Application{
     private String authToken = "";
+    public List<CustomerDetails> customerDetails;
 
     public String getAuthToken() {
         return authToken;
--- MCheque/app/src/main/java/com/example/home/mcheque/login/LoginWebServiceCall.java
+++ MCheque/app/src/main/java/com/example/home/mcheque/login/LoginWebServiceCall.java
@@ -6,6 +6,7 @@ public class LoginWebServiceCall {
     private LoginWebServiceCall() {}
 
     public static final String BASE_URL = "https://corporateapiprojectwar.mybluemix.net/corporate_banking/";
+//https://corporateapiprojectwar.mybluemix.net/corporate_banking/mybank/authenticate_client
 
     public static UserAuthenticationInterface getUserAuthenticationService() {
         return RetrofitClient.getClient(BASE_URL).create(UserAuthenticationInterface.class);
